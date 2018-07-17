d8915906bee2893815d0ff45c49e0079618c8356
牛虹 <niuhong@idss-cn.com>
2018-07-17 14:51:59 +0800

    更新数据字典_2018-07-17 14:51:59

--- git.sh
+++ git.sh
@@ -4,7 +4,7 @@ git add .
 DATE=`date "+%Y-%m-%d %H:%M:%S"`
 git commit -m "更新数据字典_${DATE}"
 git push
-scp getDataDictionary.py /idss/tools/showdoc/gitProject/0.55/data-dictionary
-cd /idss/tools/showdoc/gitProject/0.55/data-dictionary/ && sh git.sh
-scp getDataDictionary.py /idss/tools/showdoc/gitProject/10.8/doc/04.详细设计/data-dictionary
-cd /idss/tools/showdoc/gitProject/10.8/doc/04.详细设计/data-dictionary/ && sh git.sh
+scp getDataDictionary.py /idss/tools/gitLab/gitProject/0.55/data-dictionary
+cd /idss/tools/gitLab/gitProject/0.55/data-dictionary/ && sh git.sh
+scp getDataDictionary.py /idss/tools/gitLab/gitProject/10.8/doc/04.详细设计/data-dictionary
+cd /idss/tools/gitLab/gitProject/10.8/doc/04.详细设计/data-dictionary/ && sh git.sh
